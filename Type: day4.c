754681c355d6d0aa8678aa409897cd0c7587d53e
jahnavi-codes-2026 <jahnavic079@gmail.com>
2026-09-20 15:10:58 +0530

    Type: day4.c

---
+++ Type: day4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(0 {
+int a,b;
+printf("enter two numbers");
+scanf("%d %d",&a,&b);
+if (a>b) (
+  printf("%d is greater",a);
+}
+else {
+printf("%d is greater",b);
+}
+return 0:
+  }
